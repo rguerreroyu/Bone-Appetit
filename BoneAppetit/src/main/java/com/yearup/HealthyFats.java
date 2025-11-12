426d0bc0fef1a9027198d922a0ef5a8fa8db9214
RobertoGG <rguerreroyu@gmail.com>
2025-11-12 10:57:19 -0800

    Implimented healthy fats(#9)

--- BoneAppetit/src/main/java/com/yearup/HealthyFats.java
+++ BoneAppetit/src/main/java/com/yearup/HealthyFats.java
@@ -1,4 +1,70 @@
 package com.yearup;
 
 public class HealthyFats {
+    String name;
+    double price;
+    boolean extra;
+
+    public HealthyFats(String name, double price, boolean extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+
+    public HealthyFats(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(String Size) {
+        switch (Size) {
+            case "S":
+                if (extra){
+                    return 1.50;
+                }else {
+                    return 1.0;
+                }
+
+
+            case "M":
+                if (extra){
+                    return 3.0;
+                }else {
+                    return 2.0;
+                }
+
+            case "L":
+                if (extra){
+                    return 4.50;
+                }else {
+                    return 3.0;
+                }
+            default:
+                return 0;
+
+        }
+
+
+
+
+
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
+
 }
--- BoneAppetit/src/main/java/com/yearup/YogurtTreat.java
+++ BoneAppetit/src/main/java/com/yearup/YogurtTreat.java
@@ -1,46 +1,124 @@
 package com.yearup;
 
-//public class YogurtTreat extends MenuItem {
-//    private String size;
-//    private String flaborbase;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class YogurtTreat {
+    private String size;
+    private String flaborbase;
 //    private ProteinBoosts proteinboosts;
-//    private HealthyFats healhtyfats;
+    private HealthyFats healhtyfats;
+    ArrayList<ProteinBoosts> PremiumList = new ArrayList<>();
+    ArrayList<RegularToppings> RegularList = new ArrayList<>();
 //    private RegularToppings regulartoppings;
-//    private Drizzles drizzles;
-//    private Sides sides;
-//
-//    public YogurtTreat(String name, double price, ProteinBoosts proteinboosts, HealthyFats healhtyfats, RegularToppings regulartoppings, Drizzles drizzles, Sides sides) {
-//        super("Fur-Yo", 2.0);
-//        this.proteinboosts = proteinboosts;
-//        this.healhtyfats = healhtyfats;
-//        this.regulartoppings = regulartoppings;
-//        this.drizzles = drizzles;
-//        this.sides = sides;
-//    }
-//
-//
-//    public class YogurtCalculator {
-//
-//        public static double calculateYogurtPrice(
-//                int numberOfProteinBoosters,
-//                int numberOfExtraProteinBoosters) {
-//
-//            double basePrice = 5.50;
-//            double meatPrice = 1.00;
-//            double extraMeatPrice = 0.50;
-//
-//            double total = basePrice
-//                    + (meatPrice * numberOfProteinBoosters)
-//                    + (extraMeatPrice * numberOfExtraProteinBoosters);
-//
-//            return total;
-//        }
-//
-//        public double calculatePrice() {
-//            return 0;//getBasePrice() * (weightInGrams / 100.0);
-//        }
-//
-//    }
-//
-//}
+    private Drizzles drizzles;
+    private Sides sides;
+
+    public YogurtTreat(String size, String flaborbase, HealthyFats healhtyfats, ArrayList<ProteinBoosts> premiumList, ArrayList<RegularToppings> regularList, Drizzles drizzles, Sides sides) {
+        this.size = size;
+        this.flaborbase = flaborbase;
+        this.healhtyfats = healhtyfats;
+        PremiumList = premiumList;
+        RegularList = regularList;
+        this.drizzles = drizzles;
+        this.sides = sides;
+    }
+
+    public YogurtTreat(){}
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlaborbase() {
+        return flaborbase;
+    }
+
+    public void setFlaborbase(String flaborbase) {
+        this.flaborbase = flaborbase;
+    }
+
+    public HealthyFats getHealhtyfats() {
+        return healhtyfats;
+    }
+
+    public void setHealhtyfats(HealthyFats healhtyfats) {
+        this.healhtyfats = healhtyfats;
+    }
+
+    public ArrayList<ProteinBoosts> getPremiumList() {
+        return PremiumList;
+    }
+
+    public void setPremiumList(ArrayList<ProteinBoosts> premiumList) {
+        PremiumList = premiumList;
+    }
+
+    public ArrayList<RegularToppings> getRegularList() {
+        return RegularList;
+    }
+
+    public void setRegularList(ArrayList<RegularToppings> regularList) {
+        RegularList = regularList;
+    }
+
+    public Drizzles getDrizzles() {
+        return drizzles;
+    }
+
+    public void setDrizzles(Drizzles drizzles) {
+        this.drizzles = drizzles;
+    }
+
+    public Sides getSides() {
+        return sides;
+    }
+
+    public void setSides(Sides sides) {
+        this.sides = sides;
+    }
+
+    public class YogurtCalculator {
+
+        public static double calculateYogurtPrice(
+                int numberOfProteinBoosters,
+                int numberOfExtraProteinBoosters) {
+
+            double basePrice = 5.50;
+            double meatPrice = 1.00;
+            double extraMeatPrice = 0.50;
+
+            double total = basePrice
+                    + (meatPrice * numberOfProteinBoosters)
+                    + (extraMeatPrice * numberOfExtraProteinBoosters);
+
+            return total;
+        }
+
+        public double calculatePrice() {
+            return 0;//getBasePrice() * (weightInGrams / 100.0);
+        }
+
+    }
+    public void addYogurt(){
+        System.out.println("Size");
+        Scanner scanner = new Scanner(System.in);
+        YogurtTreat order = new YogurtTreat();
+        order.setSize(scanner.nextLine());//touppercase,.totrim
+
+
+        System.out.println("Flavorbase");
+        order.setFlaborbase(scanner.nextLine());
+
+        System.out.println("");
+
+    }
+
+
+
+}
 
